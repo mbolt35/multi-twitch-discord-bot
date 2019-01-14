212e9f644e3e22365b9deb1cc5429c415fa71c08
Matt Bolt <mbolt35@gmail.com>
2019-01-14 15:38:00 -0500

    Debugging for PORT env variable

--- multi-twitch-discord-bot.go
+++ multi-twitch-discord-bot.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
+	}
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Fprintf(w, "Go Http Server Test!")
 		fmt.Println(r.Header)
 	})
+	/*
 
-	http.ListenAndServe(os.Getenv("PORT"), nil)
+
+		http.ListenAndServe(os.Getenv("PORT"), nil)
+	*/
 }
