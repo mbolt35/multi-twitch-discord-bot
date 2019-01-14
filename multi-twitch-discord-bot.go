2b773128984ca063b6a70df87f8123a9c9dcc8d0
Matt Bolt <mbolt35@gmail.com>
2019-01-14 17:06:28 -0500

    Dump Env Vars

--- multi-twitch-discord-bot.go
+++ multi-twitch-discord-bot.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	//"strings"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Go Http Server Test!")
-		fmt.Println(r.Header)
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0] + os.Getenv(pair[0]))
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Println("$PORT not set. Defaulting to 3001")
+		port = "3001"
+	}
+
+	users := [3]string{"bolt_", "lucky918", "perlj"}
+
+	http.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
+
 	})
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	http.ListenAndServe(":"+port, nil)
 }
