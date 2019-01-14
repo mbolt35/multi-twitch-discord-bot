7d183ebe20d5859de7a501e03dd1a1d1c44ebaee
Matt Bolt <mbolt35@gmail.com>
2019-01-14 15:47:32 -0500

    Plug in port

--- multi-twitch-discord-bot.go
+++ multi-twitch-discord-bot.go
@@ -8,14 +8,10 @@ import (
 )
 
 func main() {
-	fmt.Println("PORT: " + os.Getenv("PORT"))
-
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Go Http Server Test!")
 		fmt.Println(r.Header)
 	})
 
-	/*
-		http.ListenAndServe(os.Getenv("PORT"), nil)
-	*/
+	http.ListenAndServce(":"+os.Getenv("PORT"), nil)
 }
