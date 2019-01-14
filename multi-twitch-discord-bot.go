31f5d8d10116225d5d4f599d5a9b3d162eb586bd
Matt Bolt <mbolt35@gmail.com>
2019-01-14 15:40:02 -0500

    More debug

--- multi-twitch-discord-bot.go
+++ multi-twitch-discord-bot.go
@@ -8,17 +8,14 @@ import (
 )
 
 func main() {
-	for _, e := range os.Environ() {
-		pair := strings.Split(e, "=")
-		fmt.Println(pair[0])
-	}
+	fmt.Println("PORT: %s", os.Getenv("PORT"))
+
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Go Http Server Test!")
 		fmt.Println(r.Header)
 	})
+
 	/*
-
-
 		http.ListenAndServe(os.Getenv("PORT"), nil)
 	*/
 }
